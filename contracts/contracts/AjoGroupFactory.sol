c14539edd4c9ea449fa6e211ca5c01a604abcf9c
MickeeJay <mutolibabdul750@gmail.com>
2026-04-16 09:24:18 +0100

    Update factory contract

--- contracts/contracts/AjoGroupFactory.sol
+++ contracts/contracts/AjoGroupFactory.sol
@@ -1,45 +1,148 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./interfaces/IAjoFactory.sol";
 import "./AjoSavingsGroup.sol";
+import "./libraries/Counters.sol";
 
-contract AjoGroupFactory is IAjoFactory {
-    address public immutable cUSD;
-    address[] private _allGroups;
+contract AjoGroupFactory is Ownable, ReentrancyGuard, IAjoFactory {
+    using Counters for Counters.Counter;
 
-    constructor(IERC20 cUSD_) {
-        cUSD = address(cUSD_);
+    error InvalidGroupSize();
+    error InvalidContributionAmount();
+    error InvalidFrequency();
+    error InvalidPotValue();
+    error GroupNotFound();
+    error InvalidInviteCode();
+    error InvalidTokenAddress();
+
+    Counters.Counter private _groupIds;
+
+    address public cUSDToken;
+    mapping(uint256 => address) public groups;
+    mapping(address => uint256[]) public userGroups;
+    mapping(uint256 => bytes32) private _groupInviteCodes;
+    uint256 public groupCount;
+
+    uint256 public constant MAX_GROUP_SIZE = 20;
+    uint256 public constant MIN_GROUP_SIZE = 3;
+    uint256 public constant MAX_CONTRIBUTION = 50e18;
+    uint256 public constant MAX_POT_VALUE = 500e18;
+
+    constructor(address cUSDToken_) Ownable(msg.sender) {
+        if (cUSDToken_ == address(0)) {
+            revert InvalidTokenAddress();
+        }
+
+        cUSDToken = cUSDToken_;
     }
 
     function createGroup(
         string calldata name,
-        IERC20 token,
         uint256 contributionAmount,
-        uint256 maxMembers,
-        uint256 cycleDuration
-    ) external override returns (address group) {
+        uint256 frequencyInDays,
+        uint256 maxMembers
+    ) external override nonReentrant {
+        if (maxMembers < MIN_GROUP_SIZE || maxMembers > MAX_GROUP_SIZE) {
+            revert InvalidGroupSize();
+        }
+
+        if (contributionAmount == 0 || contributionAmount > MAX_CONTRIBUTION) {
+            revert InvalidContributionAmount();
+        }
+
+        if (frequencyInDays != 1 && frequencyInDays != 7 && frequencyInDays != 30) {
+            revert InvalidFrequency();
+        }
+
+        if (contributionAmount * maxMembers > MAX_POT_VALUE) {
+            revert InvalidPotValue();
+        }
+
+        uint256 groupId = _groupIds.current();
+        _groupIds.increment();
+        groupCount = _groupIds.current();
+
+        bytes32 inviteCode = keccak256(
+            abi.encodePacked(address(this), msg.sender, groupId, name, contributionAmount, frequencyInDays, maxMembers, cUSDToken)
+        );
+
+        _groupInviteCodes[groupId] = inviteCode;
+        userGroups[msg.sender].push(groupId);
+
         AjoSavingsGroup savingsGroup = new AjoSavingsGroup(
             address(this),
             name,
-            token,
+            cUSDToken,
             contributionAmount,
+            frequencyInDays,
             maxMembers,
-            cycleDuration,
-            msg.sender
+            msg.sender,
+            inviteCode
         );
 
-        group = address(savingsGroup);
-        _allGroups.push(group);
-        emit GroupCreated(group, msg.sender, name);
+        address groupAddress = address(savingsGroup);
+        groups[groupId] = groupAddress;
+        emit GroupCreated(groupId, groupAddress, msg.sender, name);
     }
 
-    function allGroups(uint256 index) external view override returns (address) {
-        return _allGroups[index];
+    function joinGroup(uint256 groupId, bytes32 inviteCode) external override nonReentrant {
+        address groupAddress = groups[groupId];
+        if (groupAddress == address(0)) {
+            revert GroupNotFound();
+        }
+
+        bytes32 storedInviteCode = _groupInviteCodes[groupId];
+        if (keccak256(abi.encodePacked(inviteCode)) != keccak256(abi.encodePacked(storedInviteCode))) {
+            revert InvalidInviteCode();
+        }
+
+        userGroups[msg.sender].push(groupId);
+        AjoSavingsGroup(groupAddress).addMember(msg.sender);
+
+        emit MemberJoined(groupId, msg.sender);
     }
 
-    function totalGroups() external view override returns (uint256) {
-        return _allGroups.length;
+    function getUserGroups(address user) external view override returns (uint256[] memory) {
+        uint256[] storage storedGroups = userGroups[user];
+        uint256[] memory results = new uint256[](storedGroups.length);
+
+        for (uint256 index = 0; index < storedGroups.length; ) {
+            results[index] = storedGroups[index];
+            unchecked {
+                ++index;
+            }
+        }
+
+        return results;
+    }
+
+    function getGroupInfo(uint256 groupId) external view override returns (address) {
+        return groups[groupId];
+    }
+
+    function getActiveGroups(uint256 offset, uint256 limit) external view override returns (uint256[] memory) {
+        if (offset >= groupCount || limit == 0) {
+            return new uint256[](0);
+        }
+
+        uint256 end = offset + limit;
+        if (end < offset || end > groupCount) {
+            end = groupCount;
+        }
+
+        uint256 resultLength = end - offset;
+        uint256[] memory activeGroups = new uint256[](resultLength);
+
+        for (uint256 index = 0; index < resultLength; ) {
+            activeGroups[index] = offset + index;
+            unchecked {
+                ++index;
+            }
+        }
+
+        return activeGroups;
     }
 }
