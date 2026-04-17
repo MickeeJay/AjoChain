a4fe3bf16a53ddf0263ad2fbb1424350f5a7b490
MickeeJay <mutolibabdul750@gmail.com>
2026-04-17 08:14:05 +0100

    Update factory interface

--- contracts/contracts/interfaces/IAjoFactory.sol
+++ contracts/contracts/interfaces/IAjoFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 interface IAjoFactory {
     event GroupCreated(uint256 indexed groupId, address indexed groupAddress, address indexed creator, string name);
