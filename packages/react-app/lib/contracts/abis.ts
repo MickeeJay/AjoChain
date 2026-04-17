51a62c57634f8fef11046a6e8ab6e51bd5b3cfcb
MickeeJay <mutolibabdul750@gmail.com>
2026-04-17 22:21:57 +0100

    feat: add IERC20 ABI

--- packages/react-app/lib/contracts/abis.ts
+++ packages/react-app/lib/contracts/abis.ts
@@ -330,3 +330,33 @@ export const CUSD_ABI = [
     outputs: [{ name: "", type: "uint256" }],
   },
 ] as const;
+
+export const IERC20_ABI = [
+  {
+    type: "function",
+    name: "allowance",
+    stateMutability: "view",
+    inputs: [
+      { name: "owner", type: "address" },
+      { name: "spender", type: "address" },
+    ],
+    outputs: [{ name: "", type: "uint256" }],
+  },
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "spender", type: "address" },
+      { name: "amount", type: "uint256" },
+    ],
+    outputs: [{ name: "", type: "bool" }],
+  },
+  {
+    type: "function",
+    name: "balanceOf",
+    stateMutability: "view",
+    inputs: [{ name: "account", type: "address" }],
+    outputs: [{ name: "", type: "uint256" }],
+  },
+] as const;
