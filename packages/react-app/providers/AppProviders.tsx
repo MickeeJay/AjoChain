fc604abc3931314a44256940090a1b914f6b426a
MickeeJay <mutolibabdul750@gmail.com>
2026-06-21 18:03:57 +0100

    remove SessionProvider from AppProviders

--- packages/react-app/providers/AppProviders.tsx
+++ packages/react-app/providers/AppProviders.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { Web3Provider } from "@/providers/Web3Provider";
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      <SessionProvider>
-        <Web3Provider>{children}</Web3Provider>
-      </SessionProvider>
+      <Web3Provider>{children}</Web3Provider>
     </ThemeProvider>
   );
 }
